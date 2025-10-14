7a63e342dfe33706d3fa0b76faa6f4e22f8d65d7
Nabib Ahamed <sujanofficial19070@gmail.com>
2025-10-14 19:20:55 +0600

    rq27p dynamic array with input

--- (Book-02) Data Structure;Rafiqul Islam sir,Adbul Mottalib/Array/rq27_2p.cpp
+++ (Book-02) Data Structure;Rafiqul Islam sir,Adbul Mottalib/Array/rq27_2p.cpp
@@ -2,18 +2,25 @@
 using namespace std;
 int main()
 {
-    int *b,i,x=888,k=3;
-    b = new int[5];
-    for(int i=0;i<6;i++)
+    
+    int *b,i,x,k,n;
+    cout<<"Enter the dynamic array size, n: ";
+    cin>>n;
+    cout<<"Enter the array elements : ";
+    b = new int[n];
+    for(int i=0;i<n;i++)
     cin>>b[i];
-//we want to insert 888 in 3rd index
-    for(i=6-1;i>=k;i--)
+    cout<<"Enter the value you want to insert, x: ";
+    cin>>x;
+    cout<<"Enter the index no, k: ";
+    cin>>k;
+    for(i=n-1;i>=k;i--)
     {
         b[i+1]=b[i];
     }
     b[k]=x;
 
-    for(i=0;i<7;i++)
+    for(i=0;i<n+1;i++)
     {
         cout<<b[i]<<" ";
     }
