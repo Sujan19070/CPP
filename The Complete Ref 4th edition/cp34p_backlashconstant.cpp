bae2f847e772417e5615224c691f668c045a30f6
Nabib Ahamed <sujanofficial19070@gmail.com>
2026-08-14 11:08:52 +0600

    adding c++ books folder

---
+++ The Complete Ref 4th edition/cp34p_backlashconstant.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    printf("\n\t\"This is a Test.\"");
+    return 0;
+}
---
+++ The Complete Ref 4th edition/cr26p.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(void)
+{
+    extern int first, last;
+    printf("%d %d",first, last);
+    return 0;
+}
+int first = 10, last = 20;
